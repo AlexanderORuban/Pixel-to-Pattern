ffd6908ecf44129ba0954af0db5e0a0afdb13987
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-10 11:13:42 -0800

    updated controller order and comments + added an empty controller test file

---
+++ server/controller.test.js
@@ -0,0 +1,1 @@
+// TODO: add tests for CRUD in controller.js here
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,23 +2,23 @@ import {getAllPatterns, getPattern, postPattern, updatePattern} from '../models/
 import { Patterns } from '../models/patterns.js';
 
 
-export const updatePatternController = async(req, res) => {
-    const patternInfo = req.body;
-    const ID = patternInfo.pattern_ID;
-    const pattern = {
-        pattern_name: patternInfo.pattern_name,
-        pattern_author: patternInfo.author,
-        description: patternInfo.description
-    }
-    try{
-        await updatePattern(ID, pattern);
-        res.status(204);
+// CREATE
+
+export const uploadPattern = async (req, res) => {
+    // expect JSON object from req body with this format:
+    // { pattern_name: "", pattern_info: {}, description: ""}
+    const pattern = req.body;
+
+    // ! TODO: add validation for name and description
+    try {
+        const patternID = await postPattern(pattern);
+        res.status(201).json(patternID);
     } catch (err) {
-        return res.status(500).json({error: err.message});
+        return res.status(500).json({ error: err.message});
     }
-    
 }
 
+// READ
 export const getAll = async(req, res) => {
     try{
         const patterns = await getAllPatterns();
@@ -37,21 +37,24 @@ export const getSpecificPattern = async(req, res) => {
     }
 }
 
-// CREATE
-
-export const uploadPattern = async (req, res) => {
-    // expect JSON object from req body with this format:
-    // { pattern_name: "", pattern_info: {}, description: ""}
-    const pattern = req.body;
-
-    // ! TODO: add validation for name and description
-    try {
-        const patternID = await postPattern(pattern);
-        res.status(201).json(patternID);
-    } catch (err) {
-        return res.status(500).json({ error: err.message});
+// UPDATE
+export const updatePatternController = async(req, res) => {
+    const patternInfo = req.body;
+    const ID = patternInfo.pattern_ID;
+    const pattern = {
+        pattern_name: patternInfo.pattern_name,
+        pattern_author: patternInfo.author,
+        description: patternInfo.description
     }
+    try{
+        await updatePattern(ID, pattern);
+        res.status(204);
+    } catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+    
 }
+
 
 // DELETE
 export const deletePattern = async (req, res) => {
