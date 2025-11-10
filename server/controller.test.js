d087a7a5e5f36bd96d3e702e64fb8c3c5a03f131
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-10 11:24:55 -0800

    planning controller test file

--- server/controller.test.js
+++ server/controller.test.js
@@ -1,1 +1,15 @@
-// TODO: add tests for CRUD in controller.js here
+// TODO: add tests for CRUD in controller.js here - this is for testing the req res objects NOT the actual database interaction, that will be in the model.test.js
+import { getAll, getSpecificPattern, uploadPattern, updatePatternController, deletePattern } from './patternController.js';
+
+// set up mock req/res objects 
+
+
+// uploadPattern test
+
+// getAll test
+
+// getSpecificPattern test
+
+// updatePatternController test
+
+// deletePattern test
