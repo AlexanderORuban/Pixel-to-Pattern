36dadc98ecf287a52abb406a6185cdb3fe2a7c16
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-11 14:53:01 -0800

    wrote eraser test

--- client/src/utils/pixelTools.test.js
+++ client/src/utils/pixelTools.test.js
@@ -7,7 +7,11 @@ describe('applyTool', () => {
         expect(result).toEqual(['#fff', '#000000', '#fff']);
     });
 
-    test.todo('eraser sets the targeted pixel back to white')
+    test('eraser sets the targeted pixel back to white', () => {
+        const pixels = ['#000000', '#ff8d2fff', '#5381ffff'];
+        const result = applyTool(pixels, 1, 'eraser', '#000000');
+        expect(result).toEqual(['#000000', '#fff', '#5381ffff']);
+    })
 
     test.todo('fillBucket replaces all pixels with the selected color')
 
