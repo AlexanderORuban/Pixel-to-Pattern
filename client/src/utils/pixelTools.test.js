a7a7aad8b83d3ef511ad3c0e77e0a9284613ff68
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-11 14:43:54 -0800

    planning pixelTool tests

--- client/src/utils/pixelTools.test.js
+++ client/src/utils/pixelTools.test.js
@@ -1,3 +1,11 @@
-test('dummy test', () => {
-    expect(true).toBe(true)
+import { applyTool } from './pixelTools';
+
+describe('applyTool', () => {
+    test.todo('pencil changes only the targeted pixel to the chosen color');
+
+    test.todo('eraser sets the targeted pixel back to white')
+
+    test.todo('fillBucket replaces all pixels with the selected color')
+
+    test.todo('returns the same array if tool is unrecognized')
 })
