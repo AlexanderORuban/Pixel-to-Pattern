6c6c75fb76fd97116b8be5b9e04275c426b6cecc
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-11 14:47:17 -0800

    wrote pencil test

--- client/src/utils/pixelTools.test.js
+++ client/src/utils/pixelTools.test.js
@@ -1,7 +1,11 @@
 import { applyTool } from './pixelTools';
 
 describe('applyTool', () => {
-    test.todo('pencil changes only the targeted pixel to the chosen color');
+    test('pencil changes only the targeted pixel to the chosen color', () => {
+        const pixels = ['#fff', '#fff', '#fff'];
+        const result = applyTool(pixels, 1, 'pencil', '#000000')
+        expect(result).toEqual(['#fff', '#000000', '#fff']);
+    });
 
     test.todo('eraser sets the targeted pixel back to white')
 
