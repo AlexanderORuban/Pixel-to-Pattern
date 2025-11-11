9bf7450862e3cc561b3a125486b2fd4fe17aa9bd
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-11-11 13:24:43 -0800

    Added a test (broken, need to debug)

--- server/tests/patterns.test.js
+++ server/tests/patterns.test.js
@@ -35,3 +35,21 @@ afterAll(async () => {
     await closeDb();
 });
 
+// tests
+describe('Pattern API integration', () => {
+    test('GET /patterns returns seeded rows', async () => {
+        const res = await request(app).get('/patterns');
+
+        // Assert
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBe(2);
+    })
+
+    // TODO: Write these tests
+    test('POST /patterns creates a new row and appears in DB', async () => {
+        expect(true).toBe(true);
+    });
+        test('DELETE /patterns/:id deletes a row', async () => {
+        expect(true).toBe(true);
+    });
+});
