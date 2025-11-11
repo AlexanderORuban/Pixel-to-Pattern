e6be41781a548b04b6d0f1506e2c11506d5db452
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-11-11 12:34:57 -0800

    Separated app from server listen logic for supertest, removed testing logic from db setup

---
+++ server/app.js
@@ -0,0 +1,27 @@
+// Define app without starting the server
+
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/router.js';
+
+export const app = express();
+
+// Resolve the project root .env path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
