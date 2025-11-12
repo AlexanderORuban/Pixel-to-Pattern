5d0863d3090e9da54dfd59b3bdd57e02b8776a5c
Augy Markham <katiefaithmarkham@gmail.com>
2025-11-12 12:53:47 -0800

    planning tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -4,6 +4,28 @@ describe('can visit the page', () => {
   })
 })
 
+
+describe('can create a new pattern', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001/create')
+    cy.contains('Pixel2Pattern')
+  })
+
+  // selet painbucket
+  it('select paintBucket', () => {
+    cy.get()    // find element
+      .click()  // interact
+      .should() // assert
+  });
+
+  // chose color
+  // dump color
+  // fill in pattern name
+  // fill in author name
+  // fill in description
+  // click generate pattern
+
+})
 // can create post
   // can view post
 // can delete post
